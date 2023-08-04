6383d2406c90da8a7828e2e7cb0a965722fa945f
Jared Swift <j.w.swift@outlook.com>
2023-08-04 23:55:20 +0100

    WIP making and checking orders.

--- tasks/coffee_shop/src/coffee_shop/phases/phase_2/states/check_order.py
+++ tasks/coffee_shop/src/coffee_shop/phases/phase_2/states/check_order.py
@@ -4,7 +4,15 @@ import rospy
 
 class CheckOrder(smach.State):
     def __init__(self, voice_controller):
-        smach.State.__init__(self, outcomes=['done'])
+        smach.State.__init__(self, outcomes=['correct', 'incorrect'])
         self.voice_controller = voice_controller
+
     def execute(self, userdata):
-        return 'done'
+        order = rospy.get_param(f"/tables/{rospy.get_param('current_table')}/order")
+
+        """
+        Determine the items
+        """
+        given_order = []
+        rospy.set_param(f"/tables/{rospy.get_param('current_table')}/given_order", given_order)
+        return 'correct' if sorted(order) == sorted(given_order) else 'incorrect'
