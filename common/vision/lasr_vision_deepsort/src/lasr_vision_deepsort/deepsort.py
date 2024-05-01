0061dfcaef1ae6ead67fcb22244413dac384ee3a
Jared Swift <j.w.swift@outlook.com>
2024-05-01 08:53:52 +0100

    feat(WIP): actually do tracking.

--- common/vision/lasr_vision_deepsort/src/lasr_vision_deepsort/deepsort.py
+++ common/vision/lasr_vision_deepsort/src/lasr_vision_deepsort/deepsort.py
@@ -14,7 +14,7 @@ def create_tracker(req: StartTrackerRequest) -> DeepSort:
     return DeepSort(
         max_age=req.max_age,
         embedder=req.embedder,
-        embedder_name=req.torchreid_embedder,
+        embedder_name=req.torchreid_embedder or None,
         polygon=req.polygon,
     )
 
@@ -22,4 +22,15 @@ def create_tracker(req: StartTrackerRequest) -> DeepSort:
 def update_tracker(
     req: UpdateTrackerRequest, tracker: DeepSort
 ) -> UpdateTrackerResponse:
+    if tracker.polygon:
+        detections = [
+            (detection.xyseg, detection.confidence, detection.name)
+            for detection in req.detections
+        ]
+    else:
+        detections = [
+            (detection.xywh, detection.confidence, detection.name)
+            for detection in req.detections
+        ]
+    tracks = tracker.update_tracks(detections)
     return UpdateTrackerResponse()
